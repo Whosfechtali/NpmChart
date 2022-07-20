b0a18bad01d30c47d103f30a1cbdf8db814f3265
Whosfechtali <77290073+Whosfechtali@users.noreply.github.com>
2022-07-20 11:03:13 +0300

    Create search.js

---
+++ search.js
@@ -0,0 +1,20 @@
+{
+  "objects": [
+    {
+      "package": {
+        "name": "",
+        "scope": "",
+        "version": "",
+        "description": "",
+        "keywords": [],
+        "date": "2022-04-29T19:33:40.441Z",
+        "links": {
+          "npm": "https://www.npmjs.com/package/express",
+          "homepage": "http://expressjs.com/",
+          "repository": "https://github.com/expressjs/express",
+          "bugs": "https://github.com/expressjs/express/issues"
+        },
+      }
+
+      .package.links.
+      https://registry.npmjs.org/-/v1/search?text=express
